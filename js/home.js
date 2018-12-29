e221a943c35ed4f7b223f2e58999cc58940671b3
Mohammad Yaseen <yaseenleo>
2018-12-29 16:30:31 +0500

    data render complete of donor acceptor

--- js/home.js
+++ js/home.js
@@ -1,30 +1,59 @@
 console.log("home")
 let acceptorArray = [];
 let donorArray = [];
-function info(accep){
-     acceptorArray.forEach((acceptor)=>{
-         if(acceptor.name === accep)
-         {
-             alert(JSON.stringify(acceptor));
-         }
-     })
-    }
-   
-    function show_acceptors(){
-        let str = '<table ><tbody>';
-        acceptorArray.forEach((acceptor)=>{
-            let user  = JSON.stringify(acceptor);
-            str += `
+function info(accep) {
+    acceptorArray.forEach((acceptor) => {
+        if (acceptor.name === accep) {
+            $('#userinfo').modal('show');
+
+            let acceptName = acceptor.name;
+            let acceptEmail = acceptor.email;
+            let acceptAge = acceptor.age;
+            let acceptGender = acceptor.gender;
+            let acceptBlood = acceptor.blood;
+            let acceptAction = acceptor.action;
+            let acceptContact = acceptor.PhoneNumber;
+            let acceptHead = `${acceptAction} Details`;
+
+            document.getElementById("userAction").innerHTML = acceptHead;
+            document.getElementById("username").innerHTML = acceptName;
+            document.getElementById("useremail").innerHTML = acceptEmail;
+            document.getElementById("userage").innerHTML = acceptAge;
+            document.getElementById("usergender").innerHTML = acceptGender;
+            document.getElementById("userblood").innerHTML = acceptBlood;
+            document.getElementById("useraction").innerHTML = acceptAction;
+            document.getElementById("usercontact").innerHTML = acceptContact;
+            // alert(JSON.stringify(acceptor));
+        }
+    })
+}
+
+function donate(accep) {
+    acceptorArray.forEach((acceptor) => {
+        if (acceptor.name === accep) {
+           donorArray.push(acceptor)
+           
+           console.log(donorArray);
+        }
+    })
+}
+
+function show_acceptors() {
+    let str = '<table ><tbody>';
+    acceptorArray.forEach((acceptor) => {
+        let user = JSON.stringify(acceptor);
+        console.log(user)
+        str += `
             <tr>
-            <th>`+acceptor.name+`</th>
-            <td><button onclick="info('`+acceptor.name+`')">Info</button></td>
-            <td><button onclick="donate('`+acceptor.name+`')">Donate</button></td>
+            <th>`+ acceptor.name + `</th>
+            <td><button onclick="info('`+ acceptor.name + `')">Info</button></td>
+            <td><button onclick="donate('`+ acceptor.name + `')">Donate</button></td>
             </tr>
             `
-        })
-        str += '</tbody></table>';
-        document.getElementById("acceptors").innerHTML = str;
-    }
+    })
+    str += '</tbody></table>';
+    document.getElementById("acceptors").innerHTML = str;
+}
 firebase.auth().onAuthStateChanged(function (user) {
     if (user) {
         // User is signed in.
@@ -45,50 +74,50 @@ firebase.auth().onAuthStateChanged(function (user) {
         console.log(uid);
         console.log(providerData);
         //
-           let ref =  firebase.database().ref('donorUsers/'+uid);
-           let ref2 = firebase.database().ref('acceptorUsers/'+uid);
-          
-           
-           ref.on('value',function(snapshot){
-                
-                    
-                    let user = snapshot.val();
+        let refDonor = firebase.database().ref('donorUsers/' + uid);
+        let refAcceptor = firebase.database().ref('acceptorUsers/' + uid);
 
-                    if(user!== null){
-                        // donor user code
-                        console.log("donor,"+JSON.stringify(user));
-                        console.log(user.name)
-                        document.getElementById("name").innerHTML = user.name;
-                        let allacceptor = firebase.database().ref('acceptorUsers/');
-                        allacceptor.on('value',function(snapshot){
-                            let arr = snapshot.val();
-                            for(key in arr)
-                            {    arr[key]._uid = key
-                                console.log("something true,"+JSON.stringify(arr[key]));
-                                acceptorArray.push(arr[key]);
-                                show_acceptors();
-                            }
-                           // console.log(arr);
-                        })
-                    }
 
-            
-           })
-           ref2.on('value',function(snapshot){
-            
+        refDonor.on('value', function (snapshot) {
+
+
             let user = snapshot.val();
-                if(user!== null){
-                                           //acceptor user code
 
-                console.log("acceptor,"+JSON.stringify(user));
+            if (user !== null) {
+                // donor user code
+                console.log("donor," + JSON.stringify(user));
+                console.log(user.name)
+                document.getElementById("name").innerHTML = user.name;
+                let allacceptor = firebase.database().ref('acceptorUsers/');
+                allacceptor.on('value', function (snapshot) {
+                    let arr = snapshot.val();
+                    for (key in arr) {
+                        arr[key]._uid = key
+                        console.log("something true," + JSON.stringify(arr[key]));
+                        acceptorArray.push(arr[key]);
+                        show_acceptors();
+                    }
+                    // console.log(arr);
+                })
+            }
+
+
+        })
+        refAcceptor.on('value', function (snapshot) {
+
+            let user = snapshot.val();
+            if (user !== null) {
+                //acceptor user code
+
+                console.log("acceptor," + JSON.stringify(user));
                 console.log(user.name);
                 document.getElementById("name").innerHTML = user.name;
 
-                }
-                 
+            }
 
 
-            
+
+
         })
 
     } else {
